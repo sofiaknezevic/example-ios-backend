f9beaf6b3bf09f8041a8896696eca9e177eb3a8f
sofiaknezevic <sofia.knezevic@gmail.com>
2017-03-29 14:19:45 -0700

    Everything works, but too much extra code that I don't need

--- web.rb
+++ web.rb
@@ -20,6 +20,8 @@ post '/charge' do
   # Get the credit card details submitted by the form
   source = params[:source]
 
+  puts "inside charge, #{source}"
+
   # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
@@ -45,15 +47,68 @@ get '/customer' do
   @customer.to_json
 end
 
+post '/create_token' do
+
+  number = params[:number]
+  exp_month = params[:exp_month]
+  exp_year = params[:exp_year]
+  cvc = params[:cvc]
+
+  begin
+    card = Stripe::Token.create(
+      :card => {
+        :number => number,
+        :exp_month => exp_month,
+        :exp_year => exp_year,
+        :cvc => cvc
+      },
+    )
+
+  rescue Stripe::StripeError => e
+    status 402
+    return "Error creating charge: #{e.message}"
+  end
+
+  status 200
+  return JSON.generate(card)
+
+end
+
+post '/charge_connected_account' do
+
+  amount = params[:amount]
+  currency = params[:currency]
+  source = params[:source]
+  stripe_account = params[:stripe_account]
+
+
+  begin
+    charge = Stripe::Charge.create({
+      :amount => amount,
+      :currency => currency,
+      :source => source,
+      }, :stripe_account => stripe_account)
+
+    rescue Stripe::StripeError => e
+      status 402
+      return "Error creating charge: #{e.message}"
+  end
+
+  status 200
+  return JSON.generate(charge)
+
+end
+
 post '/customer/sources' do
   authenticate!
   source = params[:source]
 
+puts "inside customer/sources #{source}"
   # Adds the token to the customer's sources
   begin
     @customer.sources.create({:source => source})
   rescue Stripe::StripeError => e
-    status 402
+    status 406
     return "Error adding token to customer: #{e.message}"
   end
 
@@ -108,6 +163,8 @@ post '/create_charge' do
       :source => params[:source],
       :description => "Example Charge"
     )
+
+    puts "inside create_charge, #{source}"
   rescue Stripe::StripeError => e
     status 402
     return "Error creating charge: #{e.message}"
