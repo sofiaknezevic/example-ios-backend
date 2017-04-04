bba1e0ceb9cc93e719677f6ac669f575d2b11775
sofiaknezevic <sofia.knezevic@gmail.com>
2017-04-04 16:14:34 -0700

    Final product

--- web.rb
+++ web.rb
@@ -12,7 +12,7 @@ use Rack::Session::EncryptedCookie,
 
 get '/' do
   status 200
-  return "Great, your backend is set up. Now you can configure the Stripe example iOS apps to point here."
+  return "Great, your backend is set up."
 end
 
 post '/charge' do
@@ -20,13 +20,11 @@ post '/charge' do
   # Get the credit card details submitted by the form
   source = params[:source]
 
-  puts "inside charge, #{source}"
-
   # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
       :amount => params[:amount], # this number should be in cents
-      :currency => "usd",
+      :currency => "cad",
       :customer => @customer.id,
       :source => source,
       :description => "Example Charge"
@@ -99,59 +97,58 @@ post '/charge_connected_account' do
 
 end
 
-post '/customer/sources' do
-  authenticate!
-  source = params[:source]
+# post '/customer/sources' do
+#   authenticate!
+#   source = params[:source]
+#
+#   # Adds the token to the customer's sources
+#   begin
+#     @customer.sources.create({:source => source})
+#   rescue Stripe::StripeError => e
+#     status 406
+#     return "Error adding token to customer: #{e.message}"
+#   end
+#
+#   status 200
+#   return "Successfully added source."
+# end
 
-puts "inside customer/sources #{source}"
-  # Adds the token to the customer's sources
-  begin
-    @customer.sources.create({:source => source})
-  rescue Stripe::StripeError => e
-    status 406
-    return "Error adding token to customer: #{e.message}"
-  end
+# post '/customer/default_source' do
+#   authenticate!
+#   source = params[:source]
+#
+#   # Sets the customer's default source
+#   begin
+#     @customer.default_source = source
+#     @customer.save
+#   rescue Stripe::StripeError => e
+#     status 402
+#     return "Error selecting default source: #{e.message}"
+#   end
+#
+#   status 200
+#   return "Successfully selected default source."
+# end
 
-  status 200
-  return "Successfully added source."
-end
-
-post '/customer/default_source' do
-  authenticate!
-  source = params[:source]
-
-  # Sets the customer's default source
-  begin
-    @customer.default_source = source
-    @customer.save
-  rescue Stripe::StripeError => e
-    status 402
-    return "Error selecting default source: #{e.message}"
-  end
-
-  status 200
-  return "Successfully selected default source."
-end
-
-def authenticate!
-  # This code simulates "loading the Stripe customer for your current session".
-  # Your own logic will likely look very different.
-  return @customer if @customer
-  if session.has_key?(:customer_id)
-    customer_id = session[:customer_id]
-    begin
-      @customer = Stripe::Customer.retrieve(customer_id)
-    rescue Stripe::InvalidRequestError
-    end
-  else
-    begin
-      @customer = Stripe::Customer.create(:description => "iOS SDK example customer")
-    rescue Stripe::InvalidRequestError
-    end
-    session[:customer_id] = @customer.id
-  end
-  @customer
-end
+# def authenticate!
+#   # This code simulates "loading the Stripe customer for your current session".
+#   # Your own logic will likely look very different.
+#   return @customer if @customer
+#   if session.has_key?(:customer_id)
+#     customer_id = session[:customer_id]
+#     begin
+#       @customer = Stripe::Customer.retrieve(customer_id)
+#     rescue Stripe::InvalidRequestError
+#     end
+#   else
+#     begin
+#       @customer = Stripe::Customer.create(:description => "iOS SDK example customer")
+#     rescue Stripe::InvalidRequestError
+#     end
+#     session[:customer_id] = @customer.id
+#   end
+#   @customer
+# end
 
 # This endpoint is used by the Obj-C example app to create a charge.
 post '/create_charge' do
