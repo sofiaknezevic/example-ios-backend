833954337e0a4a4494ceb8551219c6155b03d60c
sofiaknezevic <sofia.knezevic@gmail.com>
2017-04-12 22:54:20 -0700

    trying to re-create error message

--- web.rb
+++ web.rb
@@ -16,35 +16,35 @@ get '/' do
   return "Great, your backend is set up."
 end
 
-# post '/charge' do
-#   authenticate!
-#   # Get the credit card details submitted by the form
-#   source = params[:source]
-#
-#   # Create the charge on Stripe's servers - this will charge the user's card
-#   begin
-#     charge = Stripe::Charge.create(
-#       :amount => params[:amount], # this number should be in cents
-#       :currency => "cad",
-#       :customer => @customer.id,
-#       :source => source,
-#       :description => "Charging"
-#     )
-#   rescue Stripe::StripeError => e
-#     status 402
-#     return "Error creating charge: #{e.message}"
-#   end
-#
-#   status 200
-#   return "Charge successfully created"
-# end
-#
-# get '/customer' do
-#   authenticate!
-#   status 200
-#   content_type :json
-#   @customer.to_json
-# end
+post '/charge' do
+  authenticate!
+  # Get the credit card details submitted by the form
+  source = params[:source]
+
+  # Create the charge on Stripe's servers - this will charge the user's card
+  begin
+    charge = Stripe::Charge.create(
+      :amount => params[:amount], # this number should be in cents
+      :currency => "cad",
+      :customer => @customer.id,
+      :source => source,
+      :description => "Charging"
+    )
+  rescue Stripe::StripeError => e
+    status 402
+    return "Error creating charge: #{e.message}"
+  end
+
+  status 200
+  return "Charge successfully created"
+end
+
+get '/customer' do
+  authenticate!
+  status 200
+  content_type :json
+  @customer.to_json
+end
 
 post '/create_token' do
 
@@ -88,7 +88,7 @@ post '/charge_connected_account' do
         :amount => amount,
         :currency => currency,
         :source => source
-      }, :stripe_account => stripe_account )
+        :stripe_account => stripe_account})
 
     rescue Stripe::StripeError => e
       status 402
