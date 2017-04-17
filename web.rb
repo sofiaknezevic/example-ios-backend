58feaeec439a6cd5ee93a60c32251fb5b417dc8c
sofiaknezevic <sofia.knezevic@gmail.com>
2017-04-17 11:50:26 -0700

    New gems

--- web.rb
+++ web.rb
@@ -3,6 +3,8 @@ require 'stripe'
 require 'dotenv'
 require 'json'
 require 'encrypted_cookie'
+require 'net/http'
+require 'uri'
 
 Dotenv.load
 
@@ -14,44 +16,6 @@ Stripe.api_key = settings.secret_key
 get '/' do
   status 200
   return "Great, your backend is set up."
-end
-
-# post '/charge' do
-#   authenticate!
-#   # Get the credit card details submitted by the form
-#   source = params[:source]
-#
-#   # Create the charge on Stripe's servers - this will charge the user's card
-#   begin
-#     charge = Stripe::Charge.create(
-#       :amount => params[:amount], # this number should be in cents
-#       :currency => "cad",
-#       :customer => @customer.id,
-#       :source => source,
-#       :description => "Charging"
-#     )
-#   rescue Stripe::StripeError => e
-#     status 402
-#     return "Error creating charge: #{e.message}"
-#   end
-#
-#   status 200
-#   return "Charge successfully created"
-# end
-#
-# get '/customer' do
-#   authenticate!
-#   status 200
-#   content_type :json
-#   @customer.to_json
-# end
-
-post '/get_client_key' do
-
-client_secret = settings.secret_key
-
-return client_secret
-
 end
 
 post '/create_token' do
